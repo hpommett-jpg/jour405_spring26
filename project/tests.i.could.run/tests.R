14b21938d936f0b1de259ad381ec185e2b51bc43
hpommett-jpg <hpommett@terpmail.umd.edu>
2026-04-28 02:40:34 -0400

    tests.i.could.run.

---
+++ project/tests.i.could.run/tests.R
@@ -0,0 +1,10 @@
+Tests I could run: Based on the types of statistical tests and data analysis techniques covered in this course, I think a few of them appear appropriate for analyzing University of Maryland’s emissions data and testing the hypothesis that progress toward carbon neutrality increasingly relies on accounting mechanisms rather than direct emissions reductions.
+
+Generally, I need to expand more, as I took note of the comments about my data being too narrow for comparison. However, for this final project, I need to examine gaps between different scopes for a multitude of reasons, making me want to consider comparing 2 factors on the same graph.
+
+Most of my data set concerns change over time as UMD changes their carbon neutrality methods. A first step in the analysis is to examine trends over time using line graphs. This includes plotting Scope 1, Scope 2, and Scope 3 emissions across the 20 year time period, as well as other measures such as gross emissions, net emissions, and the gaps that are notable between them. This will be useful for identifying whether emissions are generally decreasing, and whether structural changes (like the elimination of Scope 2 emissions after 2020) align with policy shifts rather than gradual reductions.
+
+Linear regression will be absolutely necessary, considering a huge portion of my conclusion will be based off of noticing and comparing trends. It will also be useful to examine mean comparisons and proportional analysis of the impact of different scopes. 
+
+One limitation of this analysis is that some variables, particularly offsets and renewable energy credits, are not directly published in one readable dataset and has to be approximated based on the available trends on the website. Due to this limitation, the analysis is better suited for identifying patterns and relationships rather than producing exact measurements of emissions accounting.
+
